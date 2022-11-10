389d51eeac9a167ac56b9fae3b45812620de3af4
Quentin McKay <quentinmckay1@gmail.com>
2022-11-10 11:45:50 +1100

    added cables patch

--- dist/js/main.js
+++ dist/js/main.js
@@ -82,3 +82,30 @@ projects.forEach(project => {
     observer.observe(project)
 })
 // ======================================================
+
+
+document.getElementById("glcanvas").onwheel =function(){
+    window.addEventListener('DOMMouseScroll', wheel, true);
+}
+
+function patchInitialized(patch) {
+    // You can now access the patch object (patch), register variable watchers and so on
+}
+
+function patchFinishedLoading(patch) {
+    // The patch is ready now, all assets have been loaded
+}
+
+document.addEventListener('CABLES.jsLoaded', function (event) {
+    CABLES.patch = new CABLES.Patch({
+        patch: CABLES.exportedPatch,
+        "prefixAssetPath": "",
+        "assetPath": "assets/",
+        "jsPath": "js/",
+        "glCanvasId": "glcanvas",
+        "glCanvasResizeToWindow": true,
+        "onPatchLoaded": patchInitialized,
+        "onFinishedLoading": patchFinishedLoading,
+        "canvas":{"alpha":true,"premultipliedAlpha":true} // make canvas transparent
+    });
+});
